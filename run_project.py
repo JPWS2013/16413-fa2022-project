221ca9bd62ff563eb5a6503f7fcec90231d37e60
Justin Poh <justinpws@gmail.com>
2022-11-03 12:03:24 -0700

    Added execution engine class

--- run_project.py
+++ run_project.py
@@ -1,32 +1,44 @@
 import numpy as np
 
-def init_parser(problem_file, domain_file):
-    return parser
+class ExecutionEngine():
+    def __init__(self):
+        self.parser = self.init_parser()
+        self.world = self.create_world()
 
-def create_world():
-    np.set_printoptions(precision=3, suppress=True)
-    world = World(use_gui=False)
-    sugar_box = add_sugar_box(world, idx=0, counter=1, pose2d=(-0.2, 0.65, np.pi / 4))
-    spam_box = add_spam_box(world, idx=1, counter=0, pose2d=(0.2, 1.1, np.pi / 4))
+        self.location_map, self.action_map
 
-    return world
+        self.mp = MotionPlanner(world, self.action_map, self.location_map)
 
-def generate_map():
-    pass
+    def run():
+        act_plan = get_activity_plan(problem_file, domain_file)
+
+        for action in act_plan:
+            
+            motion_plan = mp.get_motion_plan(action)
+            execute(motion_plan)
+
+
+    def init_parser(problem_file, domain_file):
+        return parser
+
+    def create_world():
+        np.set_printoptions(precision=3, suppress=True)
+        world = World(use_gui=False)
+        sugar_box = add_sugar_box(world, idx=0, counter=1, pose2d=(-0.2, 0.65, np.pi / 4))
+        spam_box = add_spam_box(world, idx=1, counter=0, pose2d=(0.2, 1.1, np.pi / 4))
+
+        return world
+
+    def generate_map():
+        pass
 
 if __name__ == "__main__":
     
-    problem_file
-    domain_file
+    problem_file = ''
+    domain_file = ''
 
-    parser = init_parser(problem_file, domain_file)
-    world = create_world()
-    map_obj = generate__map()
+    engine = ExecutionEngine(problem_file, domain_file)
 
-    mp = MotionPlanner(world, action_map, location_map)
+    
 
-    act_plan = get_activity_plan(problem_file, domain_file)
-
-    for action in act_plan:
-        motion_plan = mp.get_motion_plan(action)
-        execute(motion_plan)
+    
