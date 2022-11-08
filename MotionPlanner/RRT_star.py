58c00862cd375d5b04eb2ba0996b263a640b3a9f
corwinstites <stites.corwin@gmail.com>
2022-11-08 14:26:46 -0500

    Update to motion planner.

--- MotionPlanner/RRT_star.py
+++ MotionPlanner/RRT_star.py
@@ -19,20 +19,20 @@ class MotionPlanner:
         self.goal_int = goal_int
 
     # Create a function to find the distance between two points
-    def dist(a, b):
+    def dist(self, a, b):
         diff = (a[0]-b[0], a[1]-b[1], a[2]-b[2])
         norm = np.linalg.norm(diff)
         return norm
 
     # Create a function to place x_new between x_rand and x_nearest based on d
-    def steer(a, b, d):
+    def steer(self, a, b, d):
         diff = (a[0]-b[0], a[1]-b[1], a[2]-b[2]) # Find vector between x_rand and x_nearest
         if d > np.linalg.norm(diff): # If d is greater than the magnitude of the vector make x_new x_rand
             x_new = a
         else: # Else scale the vector to the magnitude d
             coeff = d/np.linalg.norm(diff)
-            scaled_diff = (coeff*diff[0], coeff*diff[1])
-            x_new = (scaled_diff[0]+b[0], scaled_diff[1]+b[1])
+            scaled_diff = (coeff*diff[0], coeff*diff[1], coeff*diff[2])
+            x_new = (scaled_diff[0]+b[0], scaled_diff[1]+b[1], scaled_diff[2]+b[2])
         return x_new
 
     def get_nearest_node(self, point):
@@ -51,34 +51,36 @@ class MotionPlanner:
 
         return tuple(points[i])
         
-    def get_sample_fn(body, joints, custom_limits={}, **kwargs):
+    def get_sample_fn(self, body, joints, custom_limits={}, **kwargs):
         lower_limits, upper_limits = get_custom_limits(body, joints, custom_limits, circular_limits=CIRCULAR_LIMITS)
         generator = interval_generator(lower_limits, upper_limits, **kwargs)
         def fn():
             return tuple(next(generator))
         return fn
     
-    def connect_path(near_list, x_min, c_min, x_new)
+    def connect_path(self, near_list, x_min, c_min, x_new)
         for node in near_list:
             if (collison_free(node, x_new)) and (node.cost + c(node, x_new) < c_min)
                 x_min = node
                 cmin = node.cost + c(node, x_new)
         return (x_min, c_min)
 
-    def rewire(G, X_near, x_new):
+    def rewire(self, G, X_near, x_new):
+        return G
 
-    def c(start, end):
+    def c(self, start, end):
         start_vec = np.array(start)
         end_vec = np.array(end)
         dir_vec = end_vec - start_vec
         return np.linalg.norm(dir_vec)
 
-    def collision_free(a, b):
+    def collision_free(self, a, b):
+        return collision_free
 
-    def obst_free(a, b):
+    def obst_free(self, a, b):
         get_collision_data(body, link=BASE_LINK)
 
-    def retrive_path(G, end_pos, start_pos, x_new):
+    def retrive_path(self, G, end_pos, start_pos, x_new):
         current_pos = x_new
         path = [current_pos]
         while current_pos != start_pos:  
@@ -92,7 +94,7 @@ class MotionPlanner:
 
     def in_end_region(x_new)
 
-    def solve(start_pos, end_pos):
+    def solve(self, start_pos, end_pos):
         V = [self.initial_pos] # Create a list of node positions
         G = [TreeNode(start_pos, cost, None)] # Create a list of TreeNode objects
         found = 0 # Variable to keep track of if we've made it to the goal
@@ -101,7 +103,7 @@ class MotionPlanner:
                 x_rand = get_sample_fn(sample_goal=True)
             else: # Else take the random sample from somewhere in the operating area
                 x_rand = get_sample_fn()
-            x_nearest = get_nearest(x_rand)
+            x_nearest = get_nearest_node(x_rand)
             x_new = steer(x_rand, x_nearest, self.d) # Use the stter function to make x_new's position
             if ObtacleFree(xnearest, xnew):
                 X_near = self.near(G = (V, E), xnew, min{gamma_RRG*(log(card V)/ card V)^(1/d), nu}) ;
