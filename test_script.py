5e750277052423d8ffaefaad91287dc5583d6d79
Justin Poh <justinpws@gmail.com>
2022-11-11 17:08:28 -0500

    Added new features to CLI to be able to control most of the robot manually

--- test_script.py
+++ test_script.py
@@ -7,8 +7,8 @@ SUBMODULE_PATH= os.path.abspath(os.path.join(os.getcwd(), 'padm-project-2022f'))
 sys.path.append(SUBMODULE_PATH)
 sys.path.extend(os.path.join(SUBMODULE_PATH, d) for d in ['pddlstream', 'ss-pybullet'])
 
-from pybullet_tools.utils import set_pose, Pose, Point, Euler, multiply, get_pose, get_point, create_box, set_all_static, WorldSaver, create_plane, COLOR_FROM_NAME, stable_z_on_aabb, pairwise_collision, elapsed_time, get_aabb_extent, get_aabb, create_cylinder, set_point, get_function_name, wait_for_user, dump_world, set_random_seed, set_numpy_seed, get_random_seed, get_numpy_seed, set_camera, set_camera_pose, link_from_name, get_movable_joints, get_joint_name, get_joint_positions, set_joint_positions
-from pybullet_tools.utils import CIRCULAR_LIMITS, get_custom_limits, set_joint_positions, interval_generator, get_link_pose, interpolate_poses, get_collision_data
+from pybullet_tools.utils import set_pose, Pose, Point, Euler, multiply, get_pose, get_point, create_box, set_all_static, WorldSaver, create_plane, COLOR_FROM_NAME, stable_z_on_aabb, pairwise_collision, elapsed_time, get_aabb_extent, get_aabb, create_cylinder, set_point, get_function_name, wait_for_user, dump_world, set_random_seed, set_numpy_seed, get_random_seed, get_numpy_seed, set_camera, set_camera_pose, link_from_name, get_movable_joints, get_joint_name, get_joint_positions, set_joint_positions, set_joint_position
+from pybullet_tools.utils import CIRCULAR_LIMITS, get_custom_limits, set_joint_positions, interval_generator, get_link_pose, interpolate_poses, get_collision_data, body_collision
 
 from pybullet_tools.ikfast.franka_panda.ik import PANDA_INFO, FRANKA_URDF
 from pybullet_tools.ikfast.ikfast import get_ik_joints, closest_inverse_kinematics
@@ -56,40 +56,113 @@ joints = get_movable_joints(world.robot)
 print('Base Joints', [get_joint_name(world.robot, joint) for joint in world.base_joints])
 print('Arm Joints', [get_joint_name(world.robot, joint) for joint in world.arm_joints])
 sample_fn = get_sample_fn(world.robot, world.arm_joints)
-wait_for_user()
-print("Getting random sample")
-conf = sample_fn()
-print("Sample: ", conf)
 
-goal_pos = translate_linearly(world, 1.2) 
-set_joint_positions(world.robot, world.base_joints, goal_pos)
+os.system('Clear')
 
 action_option = 0
 
-while action_option != 4:
-    action_option = input("Choose an action: 1. Get current pose, 2. Set a new pose, 3. Check for collisions, 4. stop doing stuff")
-    action_option = int(action_option.strip())
-    print("Action Option Received: ", action_option)
+while action_option != 2:
+    action_option = input("\nChoose an action:\n \
+        1. Get current pose\n \
+        2. stop doing stuff\n \
+        3. Set a new pose\n \
+        4. Set individual joint angles\n \
+        5. Check for collisions\n \
+        6. Play with random samples\n \
+        7. Move base forward\n")
+    
+    try:
+        action_option = int(action_option.strip())
+    except ValueError as e:
+        print("Invalid input detected!")
+        continue
+
     if action_option == 1:
         tool_link = link_from_name(world.robot, 'panda_hand')
         start_pose = get_link_pose(world.robot, tool_link)
         print("Pose of panda_hand: ", start_pose)
 
         joints_pos = get_joint_positions(world.robot, world.arm_joints)
-        print("Position of joints: ", joints_pos)
+        arm_joint_names = [get_joint_name(world.robot, joint) for joint in world.arm_joints]
+        print("Position of joints: ")
+        for joint, pos in zip(arm_joint_names, joints_pos):
+            print("    ", joint, ":", pos)
 
-    elif action_option ==2:
+    elif action_option == 2:
+        continue
+
+    elif action_option == 3:
         joint_angles_str = input("Feed a 7 element tuple of joint angles: ")
         joint_angles_str = joint_angles_str.split(',')
         joint_angles = tuple([float(element) for element in joint_angles_str])
         set_joint_positions(world.robot, world.arm_joints, joint_angles)
+
+    elif action_option == 4:
+        
+        valid_joint_mvmt = False
+
+        while not valid_joint_mvmt:
+            joint_num = input("Which joint do you want to move? ")
+
+            try:
+                joint_num = int(joint_num.strip())
+                assert joint_num <= 7
+                assert joint_num >= 1
+            except:
+                print("Invalid jointnumber entered")
+
+            joint_angle = input("What angle would you like the joint at? ")
+
+            try:
+                joint_angle = float(joint_angle.strip())
+            except:
+                print("Invalid joint angle entered")
+
+            joint_obj = joint_from_name(world.robot, ('panda_joint'+str(joint_num)))
+
+            # set_joint_position(world.robot, joint_obj, joint_angle)
+
+            try:
+                set_joint_position(world.robot, joint_obj, joint_angle)
+                valid_joint_mvmt = True
+            except:
+                print("Unable to set joint angle!")
+
     
-    elif action_option == 3:
-        collisions = get_collision_data(world.robot)
-        print('Collisions:')
-        print(collisions)
+    elif action_option == 5:
+        collision_data = get_collision_data(world.robot)
+        print('get_collision_data returned:', collision_data)
+
+        body_collision_val = body_collision(world.robot, world.kitchen)
+        print('body_collision_val is:', body_collision_val)
+
+    elif action_option == 6:
+        print("Getting random sample")
+        conf = sample_fn()
+        print("Sample: ", conf)
+
+        apply_sample = input("Apply this sample to the robot arm? (y/n)")
+
+        if apply_sample == 'y':
+            set_joint_positions(world.robot, world.arm_joints, conf)
+
+    elif action_option == 7:
+
+        successful_mvmt = False
+
+        while not successful_mvmt:
+            try:
+                translation_amt = input("Indicate how much to move the base:")
+                translation_amt = float(translation_amt.strip())
+                goal_pos = translate_linearly(world, translation_amt) 
+                set_joint_positions(world.robot, world.base_joints, goal_pos)
+                successful_mvmt = True
+
+            except ValueError as e:
+                print("Invalid movement amount detected!")
+                continue
     
     else:
-        continue
+        print(action_option, " is an invalid input option! Please try again.")
 
 wait_for_user()
