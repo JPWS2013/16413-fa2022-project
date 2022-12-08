358295fa725dba493f24b76630c9beb6107f5353
corwinstites <stites.corwin@gmail.com>
2022-12-08 14:47:34 -0500

    Updated optimization code.

--- TrajOpti/solver.py
+++ TrajOpti/solver.py
@@ -9,29 +9,35 @@ import numpy as np
 
 # Declare decision variables
 
-prog = MathematicalProgram()
-p1 = prog.NewContinuousVariables(9, "x")
+class solver:
+    def __init__(self, guess_vector)
+        self.prog = MathematicalProgram()
+        p1 = self.prog.NewContinuousVariables(7, "x")
 
-def cost_fun(z, start_pos, end_pos):
-    return cos_z**2 + cos_z + sin_z
-# Add the cost evaluated with x[0] and x[1].
-cost1 = prog.AddCost(cost_fun, vars=[x[0], x[1]])
+        # Set heuristic costs
 
-# Add the constraint that p1 is on the unit circle centered at (0, 2)
-prog.AddConstraint(
-    lambda z: [z[0]**2 + (z[1]-2)**2],
-    lb=np.array([1.]),
-    ub=np.array([1.]),
-    vars=p1)
+        def cost_fun(z, start_pos, end_pos):
+            return cos_z**2 + cos_z + sin_z
+        # Add the cost evaluated with x[0] and x[1].
+        cost1 = prog.AddCost(cost_fun, vars=[x[0], x[1]])
 
-# Add the constraint that p2 is on the curve y=x*x
-prog.AddConstraint(
-    lambda z: [z[1] - z[0]**2],
-    lb=[0.],
-    ub=[0.],
-    vars=p2)
+        # Add the constraint that p1 is on the unit circle centered at (0, 2)
+        self.prog.AddConstraint(
+            lambda z: [z[0]**2 + (z[1]-2)**2],
+            lb=np.array([1.]),
+            ub=np.array([1.]),
+            vars=p1)
 
-# Set the value of p1 in initial guess to be [0, 1]
-prog.SetInitialGuess(p1, [0., 1.])
-# Set the value of p2 in initial guess to be [1, 1]
-prog.SetInitialGuess(p2, [1., 1.])
+        # Add the constraint that p2 is on the curve y=x*x
+        self.prog.AddConstraint(
+            lambda z: [z[1] - z[0]**2],
+            lb=[0.],
+            ub=[0.],
+            vars=p2)
+
+
+
+        # Set the value of p1 in initial guess to be [0, 1]
+        self.prog.SetInitialGuess(p1, [0., 1.])
+        # Set the value of p2 in initial guess to be [1, 1]
+        self.prog.SetInitialGuess(p2, [1., 1.])
